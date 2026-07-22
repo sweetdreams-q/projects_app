ac0adfee0d7d991a2e2d1a105b5428d0a509af62
Ibrahim Adeleke <adelekedotun57@gmail.com>
2026-07-22 23:26:19 +0100

    Refactor main.dart to implement a counter feature in MyHomePage and update widget_test.dart for testing the counter functionality

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,44 +1,19 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:projects_app/main.dart';
 
-const MethodChannel _pathProviderChannel = MethodChannel('plugins.flutter.io/path_provider');
-
 void main() {
-  late Directory tempDir;
-
-  setUpAll(() async {
-    TestWidgetsFlutterBinding.ensureInitialized();
-    tempDir = await Directory.systemTemp.createTemp('projects_app_widget_test_');
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
-      _pathProviderChannel,
-      (methodCall) async {
-        if (methodCall.method == 'getApplicationDocumentsDirectory') {
-          return tempDir.path;
-        }
-        return null;
-      },
-    );
-  });
-
-  tearDownAll(() async {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
-      _pathProviderChannel,
-      null,
-    );
-    if (await tempDir.exists()) {
-      await tempDir.delete(recursive: true);
-    }
-  });
-
-  testWidgets('shows the staff list screen', (WidgetTester tester) async {
+  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
+
+    expect(find.text('0'), findsOneWidget);
+    expect(find.text('1'), findsNothing);
+
+    await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
 
-    expect(find.text('Staff'), findsOneWidget);
+    expect(find.text('0'), findsNothing);
+    expect(find.text('1'), findsOneWidget);
   });
 }
